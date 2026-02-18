072c76fa1e215d17f3d504adcd4cdf9ad015501f
Luke <lukeneedham@outlook.com>
2026-02-18 14:35:24 +0100

    header is cut-out

--- app/src/main/java/com/lukeneedham/languagetransfer/ui/feature/common/LessonHeader.kt
+++ app/src/main/java/com/lukeneedham/languagetransfer/ui/feature/common/LessonHeader.kt
@@ -4,24 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.CompositingStrategy
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -70,26 +67,42 @@ fun LessonHeader(
                     .cutOut()
             )
         }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier.weight(1f)
         ) {
-            Text(
-                text = "Lesson",
-                color = Color.DarkGray,
-                fontSize = 16.sp,
-            )
-            Spacer(modifier = Modifier.height(5.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                @Composable
+                fun Side(alpha: Float) {
+                    Box(
+                        modifier = Modifier.alpha(alpha)
+                    ) {
+                        Text(
+                            text = "#",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.cutOut()
+                        )
+                        Spacer(modifier = Modifier.width(15.dp))
+                    }
+                }
 
-            ThickText(
-                text = "$lessonNumber",
-                style = TextStyle(
-                    fontSize = 35.sp,
-                    fontWeight = FontWeight.Black,
-                ),
-                modifier = Modifier.cutOut()
-            )
-            Spacer(modifier = Modifier.height(10.dp))
+                Side(alpha = 1f)
+
+                ThickText(
+                    text = "$lessonNumber",
+                    style = TextStyle(
+                        fontSize = 35.sp,
+                        fontWeight = FontWeight.Black,
+                    ),
+                    modifier = Modifier.cutOut()
+                )
+
+                // For centering - invisible
+                Side(alpha = 0f)
+            }
         }
         Box(
             modifier = Modifier.size(sideSize)
